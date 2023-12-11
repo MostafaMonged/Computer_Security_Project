30148b387ea53dfa55f3f1c6ed72ec0c176fcb5f
Ahmed Khaled <91345253+ahmedelrweny@users.noreply.github.com>
2023-12-11 17:22:46 +0200

    almost

--- src/RSA.py
+++ src/RSA.py
@@ -1,26 +1,106 @@
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import rsa
+
+# Generate RSA key pairs for Alice and Bob
+alice_private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+
+bob_private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048,
+    backend=default_backend()
+)
+
+privateKeys = {"Alice": alice_private_key, "Bob": bob_private_key}
 
 
-def sign_with_certificate(plain_text, private_key_path, certificate_path):
-    # Load private key
-    with open(private_key_path, "rb") as key_file:
-        private_key = serialization.load_pem_private_key(
-            key_file.read(),
-            password=None,
-            backend=default_backend()
-        )
+def sign_RSA(plain_text, person):
+    private_key = privateKeys.get(person)
+    if private_key is None:
+        raise ValueError(f"No private key found for {person}")
 
-    # Load certificate
-    with open(certificate_path, "rb") as cert_file:
-        certificate = cert_file.read()
-
-    # Sign the plain text
+    # Sign the message
     signature = private_key.sign(
-        plain_text.encode('utf-8') + certificate,
-        padding.PKCS1v15(),
+        plain_text.encode('utf-8'),
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
         hashes.SHA256()
     )
 
+    # Write signature to file
+    write_to_file(f"{person}_signed_text.txt", signature)
+
     return signature
+
+
+def encrypt_RSA(plain_text, person):
+    public_key = privateKeys[person].public_key()
+
+    # Encrypt the message
+    cipher_text = public_key.encrypt(
+        plain_text.encode('utf-8'),
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    # Write cipher text to file
+    write_to_file(f"{person}_encrypted_text.txt", cipher_text)
+
+    return cipher_text
+
+
+def write_to_file(filename, data):
+    mode = 'wb' if isinstance(data, bytes) else 'w'
+    with open(filename, mode) as file:
+        file.write(data)
+
+
+def decrypt_RSA(cipher_text, person):
+    private_key = privateKeys.get(person)
+    if private_key is None:
+        raise ValueError(f"No private key found for {person}")
+
+    # Decrypt the message
+    plain_text = private_key.decrypt(
+        cipher_text,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+
+    # Write decrypted text to file
+    write_to_file(f"{person}_decrypted_text.txt", plain_text)
+
+
+def verify_RSA(signature, message, person):
+    public_key = privateKeys[person].public_key()
+
+    try:
+        # Verify the signature
+        public_key.verify(
+            signature,
+            message.encode('utf-8'),
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
+
+        write_to_file(f"{person}_verified_text.txt",
+                      f"Verification successful." + "\n" + f"Message: \"{message}\" is authentic.")
+
+    except Exception as e:
+        write_to_file(f"{person}_verified_text.txt", f"{person} - Verification failed. {e}")
--- src/main.py
+++ src/main.py
@@ -1,2 +1,13 @@
+from src.RSA import *
+
 if __name__ == "__main__":
-    print("Hello World!")
+    # Example usage
+    message = "Hello, cryptography!"
+
+    # Signing and verifying
+    signature = sign_RSA(message, "Alice")
+    verify_RSA(signature, message, "Alice")
+
+    # Encryption and decryption
+    cipher_text = encrypt_RSA(message, "Bob")
+    decrypted_text = decrypt_RSA(cipher_text, "Bob")
