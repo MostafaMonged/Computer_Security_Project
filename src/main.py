7f7896b41adcf5f88fd0f61b230ade6f36a74e31
Omar Taman <omar.a.taman01@gmail.com>
2023-12-14 10:25:12 +0200

    Front end nearly done #2
    RSA Tab needs consultation
    Opinion on rest
    Combo or buttons?
    Next Step Action listners

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import *
 from PyQt5.uic import loadUiType
-
+# from AES import *
 # Load the .ui file and generate the corresponding class dynamically
 Ui_MainWindow, _ = loadUiType("./GUI.ui")
 terminals = ["IDENTIFIER", "NUMBER", "OP"]
@@ -16,8 +16,8 @@ class MyGUI(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
 
         # Creating the main window
-        self.setGeometry(100, 100, 850, 600)
-        self.setWindowTitle("Scanner Application")
+        self.setGeometry(100, 100, 920, 500)
+        self.setWindowTitle("Security Application")
 
         # Center the main window on the screen
         screen = QDesktopWidget().screenGeometry()
@@ -33,80 +33,80 @@ class MyGUI(QMainWindow, Ui_MainWindow):
         # Load the css
         self.setStyleSheet(open("css.css").read())
 
-        # Text box for code input
-        self.code_editor = self.textEdit1
-        self.code_editor = self.textEdit2
-        self.code_editor = self.textEdit3
-        self.code_editor = self.textEdit4
-        self.code_editor = self.textEdit5
-
-        # X action listeners
-        # Load File button action listener
-        self.Load_File_AES.clicked.connect(self.loadFile)
-
-        self.Encrypt_AES.clicked.connect(self.encryptAES)
-        self.Decrypt_AES.clicked.connect(self.decryptAES)
-
-        # Connect the currentIndexChanged signal to the slot
-        self.EncryptionMode_AES.currentIndexChanged.connect(self.on_combo_box_current_index_changed)
-
-        # Export button action listener
-        self.Export_File_AES.clicked.connect(self.exportFile)
-
-        # Clear button action listener
-        self.Clear_AES.clicked.connect(self.clear_all)
-        # ===============================================================================================
-        # AES Encryption action listeners
-        # Load File button action listener
-        self.Load_File_AES.clicked.connect(self.loadFile)
-
-        self.EncryptAES.clicked.connect(self.encryptAES)
-        self.DecryptAES.clicked.connect(self.decryptAES)
-
-        # Clear button action listener
-        self.Clear_AES.clicked.connect(self.clear_all)
-        # ===============================================================================================
-        # RSA Encryption action listener
-        # Load File button action listener
-        self.Load_File_RSA.clicked.connect(self.loadFile)
-
-        # Encrypt/Decrypt buttons action listeners
-        self.EncryptRSA.clicked.connect(self.encryptRSA)
-        self.DecryptRSA.clicked.connect(self.decryptRSA)
-
-        # Export button action listener
-        self.Export_File_RSA.clicked.connect(self.exportFile)
-
-        # Clear button action listener
-        self.Clear_RSA.clicked.connect(self.clear_all)
-        # ===============================================================================================
-        # RSA Verification action listener
-        # Load File button action listener
-        self.Load_File_Certificate.clicked.connect(self.loadFile)
-
-        # Encrypt/Decrypt buttons action listeners
-        self.SignRSA.clicked.connect(self.signRSA)
-        self.VerifyRSA.clicked.connect(self.verifyRSA)
-
-        # Export button action listener
-        self.Export_File_Certificate.clicked.connect(self.exportFile)
-
-        # Clear button action listener
-        self.Clear_Certificate.clicked.connect(self.clear_all)
-        # ===============================================================================================
-        # Sha-512 Hashing action listener
-        # Load File button action listener
-        self.Load_File_SHA.clicked.connect(self.loadFile)
-
-        # Encrypt/Decrypt buttons action listeners
-        self.CalcSHA.clicked.connect(self.encryptSHA512)
-        self.VerifySHA.clicked.connect(self.decryptSHA512)
-
-        # Export button action listener
-        self.Export_File_SHA.clicked.connect(self.exportFile)
-
-        # Clear button action listener
-        self.Clear_SHA.clicked.connect(self.clear_all)
+        # # Text box for code input
+        # self.code_editor = self.textEdit1
+        # self.code_editor = self.textEdit2
+        # self.code_editor = self.textEdit3
+        # self.code_editor = self.textEdit4
+        # self.code_editor = self.textEdit5
+        #
+        # # X action listeners
+        # # Load File button action listener
+        # self.Load_File_AES.clicked.connect(self.loadFile)
+        #
+        # self.Encrypt_AES.clicked.connect(self.encryptAES)
+        # self.Decrypt_AES.clicked.connect(self.decryptAES)
+        #
+        # # Connect the currentIndexChanged signal to the slot
+        # # self.EncryptionMode_AES.currentIndexChanged.connect(self.on_combo_box_current_index_changed)
+        #
+        # # Export button action listener
+        # # self.Export_File_AES.clicked.connect(self.exportFile)
+        #
+        # # Clear button action listener
+        # self.Clear_AES.clicked.connect(self.clear_all)
+        # # ===============================================================================================
+        # # AES Encryption action listeners
+        # # Load File button action listener
+        # self.Load_File_AES.clicked.connect(self.loadFile)
+        #
+        # self.EncryptAES.clicked.connect(self.encryptAES)
+        # self.DecryptAES.clicked.connect(self.decryptAES)
+        #
+        # # Clear button action listener
+        # self.Clear_AES.clicked.connect(self.clear_all)
+        # # ===============================================================================================
+        # # RSA Encryption action listener
+        # # Load File button action listener
+        # self.Load_File_RSA.clicked.connect(self.loadFile)
+        #
+        # # Encrypt/Decrypt buttons action listeners
+        # self.EncryptRSA.clicked.connect(self.encryptRSA)
+        # self.DecryptRSA.clicked.connect(self.decryptRSA)
+        #
+        # # Export button action listener
+        # self.Export_File_RSA.clicked.connect(self.exportFile)
+        #
+        # # Clear button action listener
+        # self.Clear_RSA.clicked.connect(self.clear_all)
+        # # ===============================================================================================
+        # # RSA Verification action listener
+        # # Load File button action listener
+        # self.Load_File_Certificate.clicked.connect(self.loadFile)
+        #
+        # # Encrypt/Decrypt buttons action listeners
+        # self.SignRSA.clicked.connect(self.signRSA)
+        # self.VerifyRSA.clicked.connect(self.verifyRSA)
+        #
+        # # Export button action listener
+        # self.Export_File_Certificate.clicked.connect(self.exportFile)
+        #
+        # # Clear button action listener
+        # self.Clear_Certificate.clicked.connect(self.clear_all)
+        # # ===============================================================================================
+        # # Sha-512 Hashing action listener
+        # # Load File button action listener
+        # self.Load_File_SHA.clicked.connect(self.loadFile)
+        #
+        # # Encrypt/Decrypt buttons action listeners
+        # self.CalcSHA.clicked.connect(self.encryptSHA512)
+        # self.VerifySHA.clicked.connect(self.decryptSHA512)
+        #
+        # # Export button action listener
+        # self.Export_File_SHA.clicked.connect(self.exportFile)
+        #
+        # # Clear button action listener
+        # self.Clear_SHA.clicked.connect(self.clear_all)
 
         # # Table for output display
         # self.output_table = self.tableWidget1
